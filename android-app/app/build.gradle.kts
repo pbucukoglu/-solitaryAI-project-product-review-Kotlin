08402755a23b40e5c294d361cc121a219e3d6aba
pbucukoglu <pbucukoglu@gmail.com>
2026-01-16 19:25:39 +0300

    removed  unnecessary tests

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -102,10 +102,6 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     kapt("androidx.hilt:hilt-compiler:1.2.0")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.1.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
